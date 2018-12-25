802007d47a6fdcdf347b181203117763f76eb5f7
Rain Liu <yuliu@outlook.com>
2018-12-24 22:03:37 -0800

    add parse obu header

--- src/bitreader.rs
+++ src/bitreader.rs
@@ -1,17 +1,23 @@
-use std::io;
-use std::io::{Error, ErrorKind};
+use super::global::*;
 
-struct BitReader<'a> {
-    bit_buffer: &'a mut [u8],
+pub struct BitReader<'a> {
+    bit_buffer: &'a [u8],
     bit_offset: usize,
 }
 
 impl<'a> BitReader<'a> {
-    fn bytes_read(&self) -> usize {
+    pub fn new(buffer: &[u8], offset: usize) -> BitReader {
+        BitReader {
+            bit_buffer: buffer,
+            bit_offset: offset,
+        }
+    }
+
+    pub fn bytes_read(&self) -> usize {
         return (self.bit_offset + 7) >> 3;
     }
 
-    fn read_bit(&mut self) -> io::Result<i32> {
+    pub fn read_bit(&mut self) -> Result<i32, aom_codec_err_t> {
         let off = self.bit_offset;
         let p = off >> 3;
         let q = 7 - (off & 0x7) as usize;
@@ -20,14 +26,11 @@ impl<'a> BitReader<'a> {
             self.bit_offset = off + 1;
             Ok(bit)
         } else {
-            Err(Error::new(
-                ErrorKind::Other,
-                "read bit beyond buffer length",
-            ))
+            Err(aom_codec_err_t::AOM_CODEC_CORRUPT_FRAME)
         }
     }
 
-    fn read_literal(&mut self, bits: i32) -> io::Result<i32> {
+    pub fn read_literal(&mut self, bits: i32) -> Result<i32, aom_codec_err_t> {
         let mut value = 0;
         for bit in (0..bits).rev() {
             match self.read_bit() {
@@ -38,7 +41,7 @@ impl<'a> BitReader<'a> {
         Ok(value)
     }
 
-    fn read_unsigned_literal(&mut self, bits: i32) -> io::Result<u32> {
+    pub fn read_unsigned_literal(&mut self, bits: i32) -> Result<u32, aom_codec_err_t> {
         let mut value = 0u32;
         for bit in (0..bits).rev() {
             match self.read_bit() {
@@ -49,7 +52,7 @@ impl<'a> BitReader<'a> {
         Ok(value)
     }
 
-    fn read_inv_signed_literal(&mut self, bits: i32) -> io::Result<i32> {
+    pub fn read_inv_signed_literal(&mut self, bits: i32) -> Result<i32, aom_codec_err_t> {
         let nbits = (std::mem::size_of::<u32>() as i32) * 8 - bits - 1;
         let value = match self.read_literal(bits + 1) {
             Ok(l) => (l as u32) << nbits,
--- src/obu.rs
+++ src/obu.rs
@@ -1,3 +1,6 @@
+use super::bitreader;
+use super::global::*;
+
 pub enum ObuType {
     ObuSequenceHeader = 1,
     ObuTemporalDelimiter = 2,
@@ -10,6 +13,23 @@ pub enum ObuType {
     ObuPadding = 15,
 }
 
+impl ObuType {
+    pub fn from(obu_type: i32) -> Option<Self> {
+        match obu_type {
+            1 => Some(ObuType::ObuSequenceHeader),
+            2 => Some(ObuType::ObuTemporalDelimiter),
+            3 => Some(ObuType::ObuFrameHeader),
+            4 => Some(ObuType::ObuTileGroup),
+            5 => Some(ObuType::ObuMetadata),
+            6 => Some(ObuType::ObuFrame),
+            7 => Some(ObuType::ObuRedundantFrameHeader),
+            8 => Some(ObuType::ObuTileList),
+            15 => Some(ObuType::ObuPadding),
+            _ => None,
+        }
+    }
+}
+
 pub enum ObuMetadataTye {
     ObuMetadataTypeAomReserved0 = 0,
     ObuMetadataTypeHdrCll = 1,
@@ -20,12 +40,71 @@ pub enum ObuMetadataTye {
 }
 
 pub struct ObuHeader {
-    size: isize,
     obu_type: ObuType,
+    extension_flag: bool,
     has_size_field: bool,
-    has_extension: bool,
-    temporal_layer_id: i32,
-    spatial_layer_id: i32,
+    temporal_id: u8,
+    spatial_id: u8,
 }
 
+// Parses OBU header and stores values in 'header'
+pub fn rav1d_parse_obu_header(rb: &mut bitreader::BitReader) -> Result<ObuHeader, aom_codec_err_t> {
+    if rb.read_bit()? != 0 {
+        // forbidden_bit must be set to 0.
+        return Err(aom_codec_err_t::AOM_CODEC_CORRUPT_FRAME);
+    }
 
+    let obu_type: ObuType;
+    if let Some(t) = ObuType::from(rb.read_literal(4)?) {
+        obu_type = t;
+    } else {
+        return Err(aom_codec_err_t::AOM_CODEC_CORRUPT_FRAME);
+    }
+
+    let extension_flag = rb.read_bit()? != 0;
+    let has_size_field = rb.read_bit()? != 0;
+
+    if rb.read_bit()? != 0 {
+        // obu_reserved_1bit must be set to 0.
+        return Err(aom_codec_err_t::AOM_CODEC_CORRUPT_FRAME);
+    }
+
+    let temporal_id: u8;
+    let spatial_id: u8;
+    if extension_flag {
+        temporal_id = rb.read_literal(3)? as u8;
+        spatial_id = rb.read_literal(2)? as u8;
+        if rb.read_literal(3)? != 0 {
+            // extension_header_reserved_3bits must be set to 0.
+            return Err(aom_codec_err_t::AOM_CODEC_CORRUPT_FRAME);
+        }
+    } else {
+        temporal_id = 0;
+        spatial_id = 0;
+    }
+
+    return Ok(ObuHeader {
+        obu_type,
+        extension_flag,
+        has_size_field,
+        temporal_id,
+        spatial_id,
+    });
+}
+
+pub fn rav1d_parse_obu(data: &[u8]) -> Result<(), aom_codec_err_t> {
+    let mut rb = bitreader::BitReader::new(data, 0);
+
+    let obu_header = rav1d_parse_obu_header(&mut rb)?;
+
+    let mut obu_size: usize = 0;
+    if obu_header.has_size_field {
+        let mut more = 1;
+        let mut i = 0;
+        while more != 0 {
+            more = rb.read_bit()?;
+        }
+    }
+
+    return Ok(());
+}
